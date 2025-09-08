ddd735edb995f85dd5638178e59f1fc00ab9c10b
THENONT123_ <Nonthaphatc@hotmail.com_>
2025-09-08 11:33:06 +0700

    .

--- src/pages/TrackerPage.tsx
+++ src/pages/TrackerPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button, Stack, Title, Divider, Container } from "@mantine/core";
 import { v4 as uuidv4 } from "uuid";
+import AddFoodModal from "../components/Modal.tsx";
 
 type FoodItem = {
   id: string;
@@ -14,18 +15,19 @@ export default function FoodTracker() {
   const [opened, setOpened] = useState(false);
   const [items, setItems] = useState<FoodItem[]>([]);
   const categories = ["Main Course", "Drink", "Dessert"];
+  const total = 0;
 
   return (
     <Container style={{ maxWidth: 600, margin: "auto", padding: 20 }}>
       <Title order={2} mb="md">
         Food Tracker
       </Title>
-      <Button>Add Food Item</Button>
+      <Button >Add Food Item</Button>
       {/* Type additional AddFoodModal here. */}
 
       <Divider my="md" />
       {/* Type additional total cost here. */}
-      <Title order={4}>Total cost: {} Baht</Title>
+      <Title order={4}>Total cost: {total} Baht</Title>
       <Stack my="sm">{/* Type additional text here. */}</Stack>
 
       <Divider my="md" />
